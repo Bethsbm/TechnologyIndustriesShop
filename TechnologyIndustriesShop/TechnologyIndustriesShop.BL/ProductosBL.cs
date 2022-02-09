c6efe0f49260b72aeb16b6928cbdbfeb9b57ca82
Maritza Sosa <maritza_sosa@live.com>
2022-02-09 00:08:57 -0600

    Agregando Lista de Productos y DataGrid

--- TechnologyIndustriesShop/TechnologyIndustriesShop.BL/ProductosBL.cs
+++ TechnologyIndustriesShop/TechnologyIndustriesShop.BL/ProductosBL.cs
@@ -8,24 +8,19 @@ namespace TechnologyIndustriesShop.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListaDeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListaDeProductos = new List<Producto>();
+        }
+
         public List<Producto> AccederProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "iPhone 13 Pro";
-            producto1.Precio = 35000;
-            
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "iPhone 13 Pro Max";
-            producto2.Precio = 50000;
-
-            var listaDeProductos = new List<Producto>();
-            listaDeProductos.Add(producto1);
-            listaDeProductos.Add(producto2);
-
-            return listaDeProductos;
+            ListaDeProductos = _contexto.Productos.ToList();
+            return ListaDeProductos;
         }
     }
 }
--- TechnologyIndustriesShop/TechnologyIndustriesShop.Win/Form1.cs
+++ TechnologyIndustriesShop/TechnologyIndustriesShop.Win/Form1.cs
@@ -16,17 +16,10 @@ namespace TechnologyIndustriesShop.Win
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
             var productosBL = new ProductosBL();
             var listaDeProductos = productosBL.AccederProductos();
 
-            foreach (var producto in listaDeProductos)
-            {
-                MessageBox.Show(producto.Descripcion);
-            }
+            listaDeProductosBindingSource.DataSource = listaDeProductos;
         }
     }
 }
