3177b4157682c5b8e3fa55fb0f0644d80ccdcd89
Maritza Sosa <maritza_sosa@live.com>
2022-02-08 18:47:00 -0600

    Creando solución y proyectos Web y WebAdmin

---
+++ TechnologyIndustriesShop/TechnologyIndustriesShop.Web/Controllers/ProductosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechnologyIndustriesShop.Web.Models;
+
+namespace TechnologyIndustriesShop.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "iPhone 13 Pro";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 1;
+            producto2.Descripcion = "iPhone 13 Pro Max";
+
+            var listaDeProductos = new List<ProductoModel>();
+            listaDeProductos.Add(producto1);
+            listaDeProductos.Add(producto2);
+
+            return View(listaDeProductos);
+        }
+    }
+}
